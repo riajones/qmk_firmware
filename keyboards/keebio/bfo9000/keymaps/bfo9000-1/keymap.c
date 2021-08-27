9099bc8be7936bd9a670ace3a5988175481b67d6
Riley Jones <riajones88@gmail.com>
2021-08-27 08:46:09 -0700

    Update keymap

--- keyboards/keebio/bfo9000/keymaps/bfo9000-1/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/bfo9000-1/keymap.c
@@ -82,11 +82,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT(
   TD(ESC_LT), KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,  KC_F6,   KC_F7,   KC_F8,         KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-  KC_GRV,     KC_1,    KC_2,    KC_3,     KC_4,     KC_5,   KC_6,    KC_7,    KC_8,          _______,  KC_9,    KC_0,    KC_MINS, KC_PLUS,  _______, _______, KC_BSPC, KC_BSPC,
+  KC_GRV,     KC_1,    KC_2,    KC_3,     KC_4,     KC_5,   KC_6,    KC_7,    KC_8,          _______,  KC_9,    KC_0,    KC_MINS, KC_EQUAL, _______, KC_MINS, KC_BSPC, KC_BSPC,
   KC_TAB,     KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,   KC_VOLD, KC_VOLU, PLAY,          KC_PGUP,  KC_LBRC, KC_RBRC, KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTL,    KC_A,    KC_S,    KC_D,     KC_F,     KC_G,   KC_F2,   KC_F12,  KC_HOME,       KC_PGDN,  KC_UP,   KC_LSFT, KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,   WS_LEFT, WS_RIGH, KC_END,        KC_LEFT,  KC_DOWN, KC_RGHT, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-  KC_LCTL,    KC_LGUI, KC_LALT, MO(MDIA), SPC_MOUS, KC_SPC, KC_ENT,  VU,      GITHUB,        _______,  KC_ENT,  KC_ENT,  KC_RGUI, MO(MDIA), MO(MDIA),KC_LEFT, KC_DOWN, KC_RIGHT
+  KC_LCTL,    KC_LGUI, KC_LALT, MO(MDIA), SPC_MOUS, KC_SPC, KC_ENT,  VU,      GITHUB,        KC_PSCR,  _______,  KC_ENT,  KC_ENT,  KC_RGUI,  MO(MDIA),KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 [OSX] = LAYOUT(
