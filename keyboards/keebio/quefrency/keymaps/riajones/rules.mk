45d9ec0caae8c12fd3e6604243e340a0d47c7832
Riley Jones <riajones88@gmail.com>
2022-12-20 16:37:07 -0800

    first draft of quefrency mappings

---
+++ keyboards/keebio/quefrency/keymaps/riajones/rules.mk
@@ -0,0 +1,3 @@
+MOUSEKEY_ENABLE = yes       # Mouse keys
+TAP_DANCE_ENABLE = yes      # # https://beta.docs.qmk.fm/using-qmk/software-features/feature_tap_dance
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
