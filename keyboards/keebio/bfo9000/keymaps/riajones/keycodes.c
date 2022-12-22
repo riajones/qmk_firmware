15d16d40364369fa3ad2448cd00d488a4ffeea4a
Riley Jones <riajones88@gmail.com>
2022-12-22 09:47:00 -0800

    add copywrite notice

---
+++ keyboards/keebio/bfo9000/keymaps/riajones/keycodes.c
@@ -0,0 +1,30 @@
+/* Copyright 2022 Riley Jones <riajones88@gmail.com> @riajones
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+enum custom_keycodes {
+    // String  Inputs
+    CU_GMAL = SAFE_RANGE,
+    CU_YTUB,
+    // Custom behavior
+    CU_MACRO,
+    // Operating System Functionality
+    CU_SCRN,
+    CU_TERM,
+    CU_TMUX,
+    CU_SPC_MOUS,
+    CU_ENT_MED,
+    NEW_SAFE_RANGE,
+};
---
+++ keyboards/keebio/bfo9000/keymaps/riajones/tapdance.c
@@ -0,0 +1,252 @@
+/* Copyright 2022 Riley Jones <riajones88@gmail.com> @riajones
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+
+//////////////////////////////////////////////////////////////////////
+//  Tap Dance Setup                                                 //
+//////////////////////////////////////////////////////////////////////
+// https://beta.docs.qmk.fm/using-qmk/software-features/feature_tap_dance#how-to-use-tap-dance-id-how-to-use
+
+// Tap Dance keycodes
+enum td_keycodes {
+    ESC_LT,    // Toggle OSX -> Double Tap Held || Escape when Pressed
+    CMD_SHIFT, // CMD When Held || Shiftble Tap Hold
+    HM_WSL,    // Home key on single press, workspace left on double press
+    END_WSR,   // End key on single press, workspace right on double press
+    TB_TBCP,   // Single press tensorboard.corp, double tap redirect to my workstation
+    TB_OSS,    // Single press tensorboard github, double tap, tensorboard local development
+    GITHUB,    // Single press inputs github, double press github pulls
+    MACRO,     // Single press starts recording, press + hold clears, double press stops recording.
+};
+
+// Define a type containing as many tapdance states as you need
+typedef enum { TD_NONE, TD_UNKNOWN, TD_SINGLE_TAP, TD_SINGLE_TAP_HOLD, TD_DOUBLE_TAP, TD_DOUBLE_TAP_HOLD } td_state_t;
+
+static td_state_t td_state;
+
+// Function to determine the current tapdance state
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) {
+            return TD_SINGLE_TAP;
+        }
+        return TD_SINGLE_TAP_HOLD;
+    }
+
+    if (state->count == 2) {
+        if (state->interrupted || !state->pressed) {
+            return TD_DOUBLE_TAP;
+        }
+        return TD_DOUBLE_TAP_HOLD;
+    }
+    return TD_UNKNOWN; // Any number higher than the maximum state value you return above
+}
+
+void esclt_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP_HOLD:
+            clear_mods();
+        case TD_SINGLE_TAP:
+        case TD_DOUBLE_TAP:
+            register_code16(KC_ESC);
+            break;
+        case TD_DOUBLE_TAP_HOLD:
+            layer_invert(OSX);
+            break;
+        default:
+            break;
+    }
+}
+
+void esclt_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_DOUBLE_TAP:
+            unregister_code16(KC_ESC);
+        default:
+            break;
+    }
+}
+
+void cmd_shift_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            register_mods(MOD_BIT(KC_LGUI));
+            break;
+        case TD_DOUBLE_TAP_HOLD:
+            register_mods(MOD_BIT(KC_LSFT));
+            break;
+        default:
+            break;
+    }
+}
+
+void cmd_shift_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            unregister_mods(MOD_BIT(KC_LGUI));
+            break;
+        case TD_DOUBLE_TAP_HOLD:
+            unregister_mods(MOD_BIT(KC_LSFT));
+            break;
+        default:
+            break;
+    }
+}
+
+void hm_wsl_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            register_code16(KC_HOME);
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            if (layer_state_is(OSX)) {
+                register_mods(MOD_BIT(KC_LCTL));
+            } else {
+                register_mods(MOD_BIT(KC_LCTL));
+                register_mods(MOD_BIT(KC_LALT));
+            }
+            register_code16(KC_LEFT);
+            break;
+        default:
+            break;
+    }
+}
+
+void hm_wsl_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            unregister_code16(KC_HOME);
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            if (layer_state_is(OSX)) {
+                unregister_mods(MOD_BIT(KC_LCTL));
+            } else {
+                unregister_mods(MOD_BIT(KC_LCTL));
+                unregister_mods(MOD_BIT(KC_LALT));
+            }
+            unregister_code16(KC_LEFT);
+            break;
+        default:
+            break;
+    }
+}
+
+void end_wsr_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            register_code16(KC_END);
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            register_code16(layer_state_is(OSX) ? OSX_WSR : LIN_WSR);
+            break;
+        default:
+            break;
+    }
+}
+
+void end_wsr_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            unregister_code16(KC_END);
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            unregister_code16(layer_state_is(OSX) ? OSX_WSR : LIN_WSR);
+            break;
+        default:
+            break;
+    }
+}
+
+void tb_corp_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            SEND_STRING("https://tensorboard-staging.corp.google.com");
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            SEND_STRING("localhost:8080");
+            break;
+        default:
+            break;
+    }
+}
+
+void tb_corp_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // No cleanup required
+}
+
+void tb_oss_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            SEND_STRING("https://github.com/tensorflow/tensorboard");
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            SEND_STRING("localhost:6006");
+            break;
+        default:
+            break;
+    }
+}
+
+void tb_oss_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // No cleanup required
+}
+
+void github_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+        case TD_SINGLE_TAP_HOLD:
+            SEND_STRING("https://github.com");
+            break;
+        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_TAP_HOLD:
+            SEND_STRING("https://github.com/pulls");
+            break;
+        default:
+            break;
+    }
+}
+
+void github_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // No cleanup required
+}
+
+// Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [ESC_LT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esclt_finished, esclt_reset), [CMD_SHIFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, cmd_shift_finished, cmd_shift_reset), [HM_WSL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, hm_wsl_finished, hm_wsl_reset), [END_WSR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, end_wsr_finished, end_wsr_reset), [TB_TBCP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tb_corp_finished, tb_corp_reset), [TB_OSS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tb_oss_finished, tb_oss_reset), [GITHUB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, github_finished, github_reset),
+};
