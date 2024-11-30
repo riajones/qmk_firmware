acc1b5664eed57d0a2ff3b5df8a775716ceb1279
Riley Jones <riajones88@gmail.com>
2024-11-29 17:25:24 -0800

    checkpoint

--- keyboards/keebio/quefrency/keymaps/riajones/config.h
+++ keyboards/keebio/quefrency/keymaps/riajones/config.h
@@ -15,3 +15,5 @@
  */
 
 #pragma once
+
+#define FORCE_NKRO
--- keyboards/keebio/quefrency/keymaps/riajones/keymap.c
+++ keyboards/keebio/quefrency/keymaps/riajones/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
     KC_F3,   KC_F4,   TD_CMOU, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_HOME,
     KC_F5,   KC_F6,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
-    KC_F7,   KC_F8,   KC_LCTL, KC_LGUI, KC_LALT, CU_MED_MOU,KC_SPC,         _______, KC_ENT,  MO(MDIA),MO(MOUS),CTL_APP, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F7,   KC_F8,   KC_LCTL, KC_LGUI, KC_LALT, MO(MOUS),KC_SPC,           _______, KC_ENT,  MO(MDIA),MO(MOUS),CTL_APP, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [MDIA] = LAYOUT_65_with_macro(
